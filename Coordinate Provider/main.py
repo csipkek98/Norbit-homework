950b0da1363c601ced7b2acfa138f891a9a200ff
SentexX <csipkekrisztian@gmail.com>
2022-11-21 11:01:49 +0100

    Add: read_cfg function and cfg variables to use in Coordinate Provider

--- Coordinate Provider/main.py
+++ Coordinate Provider/main.py
@@ -2,11 +2,23 @@ from time import sleep
 from flask import Flask
 from flask_socketio import SocketIO
 import coordiante_reader as cm
+import json
 
 
 app = Flask(__name__)
 sio = SocketIO(app, cors_allowed_origins='*')
+port = 5000
+host= "localhost"
+send_frequency = 1
 
+def read_cfg():     #   Reads in the cfg file data
+    global port, host, send_frequency
+    f = open('config.json')
+    data = json.load(f)
+    port = data["port"] if data["port"] else 5000
+    host = data["host_address"] if data["host_address"] else "localhost"
+    send_frequency = data["coordinate_sendig_frequency"] if data["coordinate_sendig_frequency"] else 1
+    f.close()
 
 def forever_thread():
     #   This thread should run forever in the background and be able to
@@ -21,9 +33,10 @@ def forever_thread():
                 coordinate = {}
                 for key in csv_data_keys:         #     build up the coordinate      
                     coordinate[key] = csv_data[key][row_index]
-                sleep(1)
+                sleep(send_frequency)
                 sio.emit("ship_data", coordinate) #     send the new coordinate through the websocket for the Express js backend
         
 if __name__ == '__main__':
+    read_cfg()
     sio.start_background_task(forever_thread)
-    sio.run(app, debug=True)
+    sio.run(app, debug=True, host=host, port=port)
